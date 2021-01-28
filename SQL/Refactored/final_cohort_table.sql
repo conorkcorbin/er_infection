1e307957698f0e983507544708e5af521061ceb9
conorkcorbin <ccorbin@stanford.edu>
2021-01-27 22:25:04 -0800

    updated to sparse to parse val from test continuous features and output feature_vocab

--- SQL/Refactored/final_cohort_table.sql
+++ SQL/Refactored/final_cohort_table.sql
@@ -12,7 +12,9 @@ FROM `mining-clinical-decisions.abx.cohort_not_infected_rules`
 
 CREATE OR REPLACE TABLE `mining-clinical-decisions.abx.final_cohort_table` AS (
 WITH joined_table AS (
-SELECT c.*, l.Cefazolin, l.Ceftriaxone, l.Cefepime, l.Zosyn, l.Vancomycin, l.Meropenem 
+SELECT c.*, l.Cefazolin, l.Ceftriaxone, l.Cefepime, l.Zosyn, l.Vancomycin, l.Meropenem,
+l.Ampicillin, l.Ciprofloxacin, l.Vancomycin_Meropenem, l.Vancomycin_Zosyn, l.Vancomycin_Ceftriaxone,
+l.Vancomycin_Cefepime
 FROM `mining-clinical-decisions.abx.final_cohort_table` c
 LEFT JOIN `mining-clinical-decisions.abx.final_ast_labels` l
 USING (pat_enc_csn_id_coded)
@@ -36,19 +38,31 @@ WHEN Vancomycin = 0 THEN 0
 ELSE NULL END Vancomycin,
 CASE WHEN Meropenem = 1 OR (Meropenem IS NULL AND not_infected = 1) THEN 1
 WHEN Meropenem = 0 THEN 0
-ELSE NULL END Meropenem
+ELSE NULL END Meropenem,
+CASE WHEN Ampicillin = 1 OR (Ampicillin IS NULL AND not_infected = 1) THEN 1
+WHEN Ampicillin = 0 THEN 0
+ELSE NULL END Ampicillin,
+CASE WHEN Ciprofloxacin = 1 OR (Ciprofloxacin IS NULL AND not_infected = 1) THEN 1
+WHEN Ciprofloxacin = 0 THEN 0
+ELSE NULL END Ciprofloxacin,
+CASE WHEN Vancomycin_Cefepime = 1 OR (Vancomycin_Cefepime IS NULL AND not_infected = 1) THEN 1
+WHEN Vancomycin_Cefepime = 0 THEN 0
+ELSE NULL END Vancomycin_Cefepime,
+CASE WHEN Vancomycin_Ceftriaxone = 1 OR (Vancomycin_Ceftriaxone IS NULL AND not_infected = 1) THEN 1
+WHEN Vancomycin_Ceftriaxone = 0 THEN 0
+ELSE NULL END Vancomycin_Ceftriaxone,
+CASE WHEN Vancomycin_Meropenem = 1 OR (Vancomycin_Meropenem IS NULL AND not_infected = 1) THEN 1
+WHEN Vancomycin_Meropenem = 0 THEN 0
+ELSE NULL END Vancomycin_Meropenem,
+CASE WHEN Vancomycin_Zosyn = 1 OR (Vancomycin_Zosyn IS NULL AND not_infected = 1) THEN 1
+WHEN Vancomycin_Zosyn = 0 THEN 0
+ELSE NULL END Vancomycin_Zosyn
+
+
+
+
 FROM joined_table)
 
 SELECT l.*, CASE WHEN l.Cefazolin IS NULL THEN 1 ELSE 0 END label_unobserved
 FROM labels l
 )
-
-
--- SELECT 
--- AVG(Cefazolin) Cefazolin,
--- AVG(Ceftriaxone) Ceftriaxone,
--- AVG(Cefepime) Cefepime,
--- AVG(Zosyn) Zosyn,
--- AVG(Vancomycin) Vancomycin,
--- AVG(Meropenem) Meropenem,
--- FROM `mining-clinical-decisions.abx.final_ast_labels` 
