fcc05ddbadc1631f6dfd0a952d01cfd301a53992
Conor Corbin <ccorbin@stanford.edu>
2021-02-18 00:05:50 +0000

    update batch drive

--- notebooks/models/batch_driver.sh
+++ notebooks/models/batch_driver.sh
@@ -1,10 +1,10 @@
 # Validation
-data_dir=/home/conorcorbin/repos/er_infection/data/ast_models_w_not_infected/
-output_dir=/home/conorcorbin/repos/er_infection/results/ast_models_w_not_infected/validation/
-model_classes=(lasso random_forest )
+data_dir=/home/conorcorbin/repos/er_infection/data/ast_models_bucket1/
+output_dir=/home/conorcorbin/repos/er_infection/results/ast_models_bucket1/validation/
+model_classes=(ridge lightgbm)
 labels=(Ampicillin Ciprofloxacin Cefazolin Ceftriaxone Cefepime Zosyn Vancomycin Meropenem 
 Vancomycin_Ceftriaxone Vancomycin_Cefepime Vancomycin_Zosyn Vancomycin_Meropenem)
-log_dir=/home/conorcorbin/logs_ast_w_no_infection/
+log_dir=/home/conorcorbin/logs_ast_bucket1/
 mkdir -p $log_dir
 
 for mc in ${model_classes[@]}
@@ -12,14 +12,14 @@ for mc in ${model_classes[@]}
     for l in ${labels[@]}
         do
         python train_model.py --model_class $mc \
-                            --data_dir $data_dir \
-                            --label $l \
-                            --output_dir ${output_dir}/${mc}/${l} \
-                            --val &> ${log_dir}${mc}_${l}_validation.txt & 
+                              --data_dir $data_dir \
+                              --label $l \
+                              --output_dir ${output_dir}/${mc}/${l} \
+                              --val &> ${log_dir}${mc}_${l}_validation.txt & 
         done
     done
 
-# Testing
+# #Testing
 # data_dir=/home/conorcorbin/repos/er_infection/data/
 # output_dir=/home/conorcorbin/repos/er_infection/results/ast_models/testing/
 # labels=(Ampicillin Ciprofloxacin Cefazolin Ceftriaxone Cefepime Zosyn Vancomycin Meropenem 
